90b31d06f5b0fab4e7cd3089ba3a94b0ab5a4916
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-10-05 04:22:31 +0900

    DiceComponent 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 const App = () => {
-  return <div>HelloWorld!</div>;
+  return (
+    <div>
+      <DiceComponent />
+    </div>
+  );
+};
+
+const DiceComponent = () => {
+  return (
+    <div>
+      <h1>주사위 컴포넌트</h1>
+      <button>클릭!</button>
+    </div>
+  );
 };
 
 export default App;
