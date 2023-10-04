d7c7c831f1570bbd2598e10c8553b9db947ebd40
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-10-05 04:19:30 +0900

    Reset App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 const App = () => {
-  return (
-    <div>
-      <MyComponent title="Component" />
-      <MyComponent title="Props" />
-      <MyComponent title="React" />
-    </div>
-  );
-};
-
-const MyComponent = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <div>부모 컴포넌트에서 넣어주는 속성에 따라 제목이 달라져요!!</div>
-    </div>
-  );
+  return <div>HelloWorld!</div>;
 };
 
 export default App;
