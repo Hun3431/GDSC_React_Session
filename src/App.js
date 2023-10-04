c0104db1b9ae47ea48c17114a01e66c25ea04635
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-10-05 03:50:45 +0900

    3. Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  return <MyComponent></MyComponent>;
+};
 
-function App() {
+const MyComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>사용자정의 컴포넌트</h1>
+      <div>마음대로 태그를 만들 수 있어요.</div>
     </div>
   );
-}
+};
 
 export default App;
