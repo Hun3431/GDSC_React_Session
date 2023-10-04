0155d59a75413d55f9f13eb789ba5e2512799b01
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-10-05 03:51:50 +0900

    5. State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-const App = () => {
-  const click = () => {
-    alert("버튼 클릭!");
-  };
+import { useState } from "react";
 
-  return <button onClick={click}>Click</button>;
+const App = () => {
+  const [count, setCount] = useState(0);
+  const click = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={click}>Click</button>
+    </div>
+  );
 };
 
 export default App;
